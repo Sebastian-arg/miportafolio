a6fc166911b6edc7ac8c40b511e61ce9c92646ec
vargasseba60 <vargasseba60@gmail.com>
2025-10-13 00:44:10 +0000

    Actualizo logo y color de fondo

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+// Modal
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-image");
+const certificateImages = document.querySelectorAll(".certificate-image");
+const closeButton = document.querySelector(".close-button");
+
+certificateImages.forEach(image => {
+  image.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.src = this.src;
+  }
+});
+
+closeButton.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
